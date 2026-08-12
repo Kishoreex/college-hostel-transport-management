24a1b111b569919954b39574210169c892e86bd6
Kishoreex <kishore1kumar4@gmail.com>
2026-08-12 14:28:47 +0530

    Update Program configuration

--- backend/HostelTransportAPI/Program.cs
+++ backend/HostelTransportAPI/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddCors(options =>
     {
         policy
             .WithOrigins(
+                  "https://campus.madhapharma.in",
                 "https://college-hostel-transport-management.vercel.app",
                 "https://college-hostel-transport-management-git-main-kishoreex1.vercel.app",
                 "http://202.61.121.102",

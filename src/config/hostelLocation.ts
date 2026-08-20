dac71c1aea3d6af296e0db635b060404ab417636
Kishoreex <kishore1kumar4@gmail.com>
2026-08-20 17:00:27 +0530

    Update hostel location configuration

--- src/config/hostelLocation.ts
+++ src/config/hostelLocation.ts
@@ -1,6 +1,6 @@
-export const HOSTEL_LOCATION = {
-  latitude: 12.993436,
-  longitude: 80.085417,
-};
 
-export const HOSTEL_RADIUS = 50;
+export const HOSTEL_LOCATION = {
+  latitude: 12.991670,
+  longitude: 80.086338,
+};
+export const HOSTEL_RADIUS = 300;
